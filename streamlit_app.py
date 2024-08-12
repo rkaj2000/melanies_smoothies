d70585114746a37e40a9dc68cc65391f6c89d019
rkaj2000 <rkaj2000@gmail.com>
2024-08-12 10:56:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
 # new section to display nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-st.text("fruityvice_response value above")
+st.text(fruityvice_response.json())
+
